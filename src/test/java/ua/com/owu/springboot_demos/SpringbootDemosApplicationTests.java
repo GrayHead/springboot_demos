a00c4dd57885a03af023fac4942e5b36854d54af
GrayHead <ss.zhuravlov@gmail.com>
2022-04-23 23:13:26 +0300

    init master

---
+++ src/test/java/ua/com/owu/springboot_demos/SpringbootDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.owu.springboot_demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
